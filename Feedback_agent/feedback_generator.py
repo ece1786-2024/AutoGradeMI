ccad059b44961162775c6fdc129ab63b87210b7d
JohnLYYN123 <96747053+JohnLYYN123@users.noreply.github.com>
2024-11-13 11:54:36 -0500

    initial build of generator is finished
    adjusting prompt and create generator API needed

--- Feedback_agent/feedback_generator.py
+++ Feedback_agent/feedback_generator.py
@@ -1,1 +1,59 @@
-# Feedback generator main
+# Feedback generator main -- use GPT-4o
+import prompt_feedback_generation as p
+from openai import OpenAI
+import pandas as pd 
+
+def main(mode="normal"):
+    mode_list = ["normal", "debug_1", "debug_5"]
+    if mode not in mode_list:
+        print("Invalid mode passed")
+        return None
+
+    # there is no training and validation involved
+    # I simply want to use the two files, and the name is only to make them distinct
+    train_path = "../data/dataset/processed/train.csv"
+    val_path = "../data/dataset/processed/validation.csv"
+
+    # for now just use validation 
+    df = pd.read_csv(val_path, encoding='utf-8')
+    if(mode == "debug_1"):
+        df = df.iloc[[0]]
+    elif(mode == "debug_5"):
+        df = df[1:6]
+
+    question_list = df["prompt"]
+    essay_list = df["essay"]
+
+    feedback_list = []
+    client = OpenAI()
+    for (q, e) in zip(question_list, essay_list):
+        user_content = p.create_user_prompt(q,e)
+        system_content = p.create_system_prompt()
+        completion = client.chat.completions.create(
+            model="gpt-4o", 
+            messages=[
+                {
+                    "role": "system", 
+                    "content": system_content
+                }, 
+                {
+                    "role": "user",
+                    "content": user_content
+                }
+            ]
+        )
+        res = completion.choices[0].message.content
+        feedback_list.append(res)
+    
+    # prep for the evaluator and human evaluator
+    # for human evaluators, the program exports
+    feedback_df = pd.DataFrame({"feedback": feedback_list})
+    df['feedback'] = feedback_df['feedback']
+
+    # export file current directory
+    df.to_csv('./feedback.csv', index=False, encoding='utf-8', mode="w")
+    return None
+
+
+if __name__ == "__main__":
+    main("debug_1")
--- Feedback_agent/prompt_feedback_generation.py
+++ Feedback_agent/prompt_feedback_generation.py
@@ -1,6 +1,36 @@
 # Feedback Generator Prompt
-import IELTS_rubrics
+import IELTS_rubrics as rubric
+identity_prompt = (
+    "You are an IELTS writing section examiner.\n"
+)
 
-print(IELTS_rubrics.BAND_SCORE)
-print(IELTS_rubrics.CRITERIA)
-print(IELTS_rubrics.BASIC_RUBRIC)
+task_prompt = """Given the following writing question and student essay, provide feedbacks for this student to improve the essay.
+The feedback must be constructive, and provide suggestions for improvements.\n  
+"""
+
+def create_system_prompt(basic_rubric=True, criteria=True, band_score=True):
+    prompt = identity_prompt
+    
+    if basic_rubric:
+        prompt += f"Rubric: {rubric.BASIC_RUBRIC}\n\n"
+    
+    if criteria:
+        prompt += f"Criteria: {rubric.CRITERIA}\n\n"
+
+    if band_score:
+        prompt += f"Band Scores (for each criteria): {rubric.BAND_SCORE}\n\n"
+
+    prompt += task_prompt
+    
+    return prompt
+
+def create_user_prompt(question, essay):
+    msg = "Here is the writing question and the essay for feedback: \n"
+    prompt = f"{msg}Question: {question}\n\nEssay: {essay}"
+    return prompt
+
+if __name__ == "__main__":
+    print(" =========== USER PROMPT SAMPLE ============= ")
+    print(create_user_prompt("Sample Question?", "Sample Essay"))
+    print("\n\n =========== SYSTEM PROMPT SAMPLE ============= ")
+    print(create_system_prompt())
