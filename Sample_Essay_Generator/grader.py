6a2873af3541fb615da07e62c879fa3551895605
Sharwarma No Sause <77422193+kbkbShang@users.noreply.github.com>
2024-11-27 00:31:55 -0500

    Updated RAG with weight

--- Sample_Essay_Generator/grader.py
+++ Sample_Essay_Generator/grader.py
@@ -1,5 +1,4 @@
 import sys
-import pandas as pd
 from openai import OpenAI
 import numpy as np
 import json
@@ -22,34 +21,6 @@ def generate_embedding(text, model="text-embedding-ada-002"):
     except Exception as e:
         print(f"Error generating embedding")
         return None
-    
-faiss_index_path = current_file.parent.parent / "RAG" / "faiss_index_train_topics.bin"
-embeddings_json_path = current_file.parent.parent / "RAG" / "embeddings_dataset_train_topics.json"
-
-index = faiss.read_index(str(faiss_index_path))
-
-with open(str(embeddings_json_path), "r", encoding="utf-8") as f:
-    metadata = json.load(f)
-    
-def search_cosine_similarity(query_text, top_k=3):
-    
-    query_embedding = generate_embedding(query_text)
-    if query_embedding is None:
-        return []
-    
-    
-    query_embedding_np = np.array([query_embedding], dtype=np.float32)
-    faiss.normalize_L2(query_embedding_np)
-    
-    distances, indices = index.search(query_embedding_np, top_k)
-    
-    
-    results = []
-    for i, idx in enumerate(indices[0]):
-        result = metadata[idx]
-        result["similarity"] = distances[0][i]  
-        results.append(result)
-    return results
 
 label_map = {
     0: '<4',
@@ -66,9 +37,62 @@ label_map = {
     11: 9.0
 }
 
-def get_score_prompt_version_RAG(topic, essay):
+def search_combined_similarity(query_prompt, query_essay,  topic_weight, essay_weight, top_k):
     
-    results = search_cosine_similarity(f"Prompt: {topic}\nEssay: {essay}", top_k=3)
+    essay_index_path = current_file.parent.parent / "RAG" / "faiss_index_train_essay.bin"
+    prompt_index_path = current_file.parent.parent / "RAG" / "faiss_index_train_topics.bin"
+    essay_embeddings_json_path = current_file.parent.parent / "RAG" / "embeddings_dataset_train_essay.json"
+    prompt_embeddings_json_path = current_file.parent.parent / "RAG" / "embeddings_dataset_train_topics.json"
+    
+    essay_index = faiss.read_index(str(essay_index_path))
+    prompt_index = faiss.read_index(str(prompt_index_path))
+    
+    with open(str(essay_embeddings_json_path), "r", encoding="utf-8") as f:
+        essay_metadata = json.load(f)
+    with open(str(prompt_embeddings_json_path), "r", encoding="utf-8") as f:
+        prompt_metadata = json.load(f)
+
+    query_prompt_embedding = generate_embedding(query_prompt)
+    query_essay_embedding = generate_embedding(query_essay)
+    
+    if query_prompt_embedding is None or query_essay_embedding is None:
+        return []
+    
+    query_prompt_embedding_np = np.array([query_prompt_embedding], dtype=np.float32)
+    query_essay_embedding_np = np.array([query_essay_embedding], dtype=np.float32)
+    faiss.normalize_L2(query_prompt_embedding_np)
+    faiss.normalize_L2(query_essay_embedding_np)
+    
+    prompt_distances, prompt_indices = prompt_index.search(query_prompt_embedding_np, len(prompt_metadata))
+    essay_distances, essay_indices = essay_index.search(query_essay_embedding_np, len(essay_metadata))
+
+    # resort according to the original indices 0,1,...
+    prompt_sort_indices = np.argsort(prompt_indices[0])
+    prompt_indices_sorted = prompt_indices[0][prompt_sort_indices]
+    prompt_distances_sorted = prompt_distances[0][prompt_sort_indices]
+    
+    essay_sort_indices = np.argsort(essay_indices[0])
+    essay_indices_sorted = essay_indices[0][essay_sort_indices]
+    essay_distances_sorted = essay_distances[0][essay_sort_indices]
+
+    #(index, pormpt_distance, essay_distance)
+    prompt_essay_pair = list(zip(essay_indices_sorted,prompt_distances_sorted,essay_distances_sorted))
+
+    sorted_prompt_essay_pair = sorted(prompt_essay_pair, key=lambda x: topic_weight * x[1] + essay_weight * x[2], reverse=True)[0:top_k]
+
+    results = []
+    for i, x in enumerate(sorted_prompt_essay_pair):
+        idx = x[0]
+        sim = topic_weight * x[1] + essay_weight * x[2]
+        result = essay_metadata[idx]
+        result["similarity"] = sim 
+        results.append(result)
+    return results
+    
+
+def get_score_prompt_version_RAG(topic, essay,topic_weight, essay_weight, top_k):
+    
+    results = search_combined_similarity(topic,essay, topic_weight=topic_weight, essay_weight=essay_weight,top_k=top_k)
 
     grader_prompt = f"""
     You are an IELTS writing section examiner, tasked with evaluating essays strictly based on the official IELTS writing rubric.
@@ -100,7 +124,7 @@ def get_score_prompt_version_RAG(topic, essay):
     - If scores vary significantly, consider revisiting the rubric alignment for the given essay.
 
     **Final Output**:
-    - Output only the final averaged score directly, only the score number, but if the score is smaller than 4, output only the '<4'. Don't be too strict on the score, be more flexible.
+    - Output only the final averaged score directly, only the score number. Don't be too strict on the score, be more flexible.
     """
     response = client.chat.completions.create(
         model="gpt-4o",
@@ -117,9 +141,9 @@ def main():
     print("\nEnter the essay text:")
     user_essay = input("> ")
 
-    #print("\nGenerating grade...")
+    print("\nGenerating grade...")
     
-    grade = get_score_prompt_version_RAG(user_topic, user_essay)
+    grade = get_score_prompt_version_RAG(user_topic, user_essay, topic_weight=0.3, essay_weight=0.7, top_k=3)
     
     if '<4' in grade:
         grade = '<4'
