22b748535f6a9e32dab0e798be6464838030f61a
Sharwarma No Sause <77422193+kbkbShang@users.noreply.github.com>
2024-11-26 21:50:18 -0500

    Connect the evaluator to integrator

--- integration/integrate.py
+++ integration/integrate.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 # Define paths to the external directories
-feedback_agent_path = Path(__file__).resolve().parent.parent / "Feedback_agent" / "Generator_merged"
+feedback_agent_path = Path(__file__).resolve().parent.parent / "Feedback_agent" / "integration"
 sample_agent_path = Path(__file__).resolve().parent.parent / "Sample_Essay_Generator"
 
 # Add paths to sys.path for importing modules
@@ -10,7 +10,7 @@ sys.path.append(str(feedback_agent_path))
 sys.path.append(str(sample_agent_path))
 
 # Import external modules
-import batch_generator_merged_version as feedback_agent
+import two_agent_Interaction_new_prompt as feedback_agent
 import grader as grader_agent 
 import sample_essay_generator as sample_agent
 
@@ -31,15 +31,19 @@ def process_essay(topic: str, essay: str, desired_score: float):
 
     # Feedback (from feedback_agent)
     feedback = feedback_agent.generate_feedback(topic, essay, predicted_score, desired_score)
-
-    # Sample essay (from sample_agent)
-    for i in range(10):
-        sample_essay = sample_agent.generate_sample_essay(topic, essay, feedback, predicted_score, desired_score)
-        sample_score = grader_agent.get_score_prompt_version_RAG(topic, sample_essay)
-        if float(sample_score) >= float(desired_score):
-            break
-        if i == 9:
-            sample_essay = "Sorry, we cannot generate a sample essay that meets the desired score base on your essay. Please continue keep on practicing. Good luck!"
+    
+    if feedback == "Failed to generate 'good' feedback within the turn limit. Please try again.":
+        sample_essay = "Sorry, we cannot generate a sample essay without a feedback. Please try again"
+        
+    else:
+        # Sample essay (from sample_agent)
+        for i in range(10):
+            sample_essay = sample_agent.generate_sample_essay(topic, essay, feedback, predicted_score, desired_score)
+            sample_score = grader_agent.get_score_prompt_version_RAG(topic, sample_essay)
+            if float(sample_score) >= float(desired_score):
+                break
+            if i == 9:
+                sample_essay = "Sorry, we cannot generate a sample essay that meets the desired score base on your essay. Please try again"
         
     # Output grade
     if '<4' in predicted_score:
